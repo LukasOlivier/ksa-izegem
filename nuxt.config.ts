1c91ee1cb336dda2e0604d0575225576efaaa95c
Lukas Olivier <olivier.lukas2003@gmail.com>
2025-09-02 19:16:50 +0200

    chore: added favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,95 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  app: {
+    head: {
+      title: 'KSA Izegem',
+      link: [
+        {
+          rel: 'apple-touch-icon',
+          sizes: '57x57',
+          href: '/apple-icon-57x57.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '60x60',
+          href: '/apple-icon-60x60.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '72x72',
+          href: '/apple-icon-72x72.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '76x76',
+          href: '/apple-icon-76x76.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '114x114',
+          href: '/apple-icon-114x114.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '120x120',
+          href: '/apple-icon-120x120.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '144x144',
+          href: '/apple-icon-144x144.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '152x152',
+          href: '/apple-icon-152x152.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-icon-180x180.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '192x192',
+          href: '/android-icon-192x192.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon-32x32.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '96x96',
+          href: '/favicon-96x96.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon-16x16.png',
+        },
+        { rel: 'manifest', href: '/manifest.json' },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+      meta: [
+        {
+          name: 'description',
+          content:
+            'Wij zijn KSA Izegem (Katholieke studenten Actie), een jeugdbeweging die al van 1927 de lach op je zoons gezicht brengt. Dit aan de hand van een zaterdagmiddag met actieve en interessante spelletjes. Uiteraard is dit niet het enigste dat wij binnen KSA doen. Wij organiseren ook: kampen, weekends, speciale activiteiten voor jong en oud, groot en klein!',
+        },
+        { name: 'msapplication-TileColor', content: '#ffffff' },
+        { name: 'msapplication-TileImage', content: '/ms-icon-144x144.png' },
+        { name: 'theme-color', content: '#ffffff' },
+      ],
+    },
+  },
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
